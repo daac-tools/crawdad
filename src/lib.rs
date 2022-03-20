6feb860eda2ea57e926ccdabbeea978a14780182
kampersanda <shnsk.knd@gmail.com>
2022-03-20 19:27:36 +0000

    add common prefix search

--- src/lib.rs
+++ src/lib.rs
@@ -18,34 +18,119 @@ impl Trie {
     where
         K: AsRef<str>,
     {
-        let mut node_id = 0;
+        let mut idx = 0;
         for c in key.as_ref().chars() {
-            if let Some(child_id) = self.get_child_id(node_id, c) {
-                node_id = child_id;
+            if let Some(mc) = self.mapper.get(c as u32) {
+                if let Some(child_id) = self.get_child_id(idx, mc) {
+                    idx = child_id;
+                } else {
+                    return None;
+                }
             } else {
                 return None;
             }
         }
-        if self.nodes[node_id as usize].is_leaf() {
-            Some(self.nodes[node_id as usize].get_base())
-        } else if self.nodes[node_id as usize].has_leaf() {
-            let leaf_id = self.nodes[node_id as usize].get_base() ^ END_CODE;
-            debug_assert_eq!(self.nodes[leaf_id as usize].get_check(), node_id);
+        if self.nodes[idx as usize].is_leaf() {
+            Some(self.nodes[idx as usize].get_base())
+        } else if self.nodes[idx as usize].has_leaf() {
+            let leaf_id = self.nodes[idx as usize].get_base() ^ END_CODE;
+            debug_assert_eq!(self.nodes[leaf_id as usize].get_check(), idx);
             Some(self.nodes[leaf_id as usize].get_base())
         } else {
             None
         }
     }
 
+    pub fn map_text<K>(&self, text: K, mapped: &mut Vec<Option<u32>>)
+    where
+        K: AsRef<str>,
+    {
+        mapped.clear();
+        for c in text.as_ref().chars() {
+            mapped.push(self.mapper.get(c as u32));
+        }
+    }
+
+    pub fn common_prefix_searcher<'k, 't>(
+        &'t self,
+        text: &'k [Option<u32>],
+    ) -> CommonPrefixSearcher<'k, 't> {
+        CommonPrefixSearcher {
+            text,
+            pos: 0,
+            trie: self,
+            idx: 0,
+        }
+    }
+
     #[inline(always)]
-    fn get_child_id(&self, node_id: u32, c: char) -> Option<u32> {
-        if self.nodes[node_id as usize].is_leaf() {
+    fn get_child_id(&self, idx: u32, mc: u32) -> Option<u32> {
+        if self.nodes[idx as usize].is_leaf() {
             return None;
         }
-        if let Some(mc) = self.mapper.get(c as u32) {
-            let child_id = self.nodes[node_id as usize].get_base() ^ mc;
-            if self.nodes[child_id as usize].get_check() == node_id {
-                return Some(child_id);
+        let child_idx = self.nodes[idx as usize].get_base() ^ mc;
+        if self.nodes[child_idx as usize].get_check() == idx {
+            return Some(child_idx);
+        }
+        None
+    }
+
+    #[inline(always)]
+    fn is_leaf(&self, idx: u32) -> bool {
+        self.nodes[idx as usize].is_leaf()
+    }
+
+    #[inline(always)]
+    fn has_leaf(&self, idx: u32) -> bool {
+        self.nodes[idx as usize].has_leaf()
+    }
+
+    #[inline(always)]
+    fn get_leaf(&self, idx: u32) -> u32 {
+        let leaf_id = self.nodes[idx as usize].get_base() ^ END_CODE;
+        debug_assert_eq!(self.nodes[leaf_id as usize].get_check(), idx);
+        leaf_id
+    }
+
+    #[inline(always)]
+    fn get_value(&self, idx: u32) -> u32 {
+        debug_assert!(self.is_leaf(idx));
+        self.nodes[idx as usize].get_base()
+    }
+}
+
+pub struct CommonPrefixSearcher<'k, 't> {
+    text: &'k [Option<u32>],
+    pos: usize,
+    trie: &'t Trie,
+    idx: u32,
+}
+
+impl Iterator for CommonPrefixSearcher<'_, '_> {
+    type Item = (u32, usize);
+
+    #[inline(always)]
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.pos < self.text.len() {
+            if let Some(mc) = self.text[self.pos] {
+                if let Some(child_idx) = self.trie.get_child_id(self.idx, mc) {
+                    self.idx = child_idx;
+                } else {
+                    self.pos = self.text.len();
+                    return None;
+                }
+            } else {
+                self.pos = self.text.len();
+                return None;
+            }
+            self.pos += 1;
+            if self.trie.is_leaf(self.idx) {
+                let matched_pos = self.pos;
+                self.pos = self.text.len();
+                return Some((self.trie.get_value(self.idx), matched_pos));
+            } else if self.trie.has_leaf(self.idx) {
+                let leaf_idx = self.trie.get_leaf(self.idx);
+                return Some((self.trie.get_value(leaf_idx), self.pos));
             }
         }
         None
@@ -91,5 +176,22 @@ mod tests {
         for (i, key) in keys.iter().enumerate() {
             assert_eq!(trie.exact_match(&key), Some(i as u32));
         }
+    }
+
+    #[test]
+    fn test_common_prefix_search() {
+        let keys = vec!["世界", "世界中", "世直し", "国民"];
+        let trie = builder::Builder::new().from_keys(&keys);
+
+        let mut text = vec![];
+        trie.map_text("世界中で世直し", &mut text);
+
+        let mut results = vec![];
+        for i in 0..text.len() {
+            for (val, pos) in trie.common_prefix_searcher(&text[i..]) {
+                results.push((val, i + pos));
+            }
+        }
+        assert_eq!(results, vec![(0, 2), (1, 3), (2, 7)]);
     }
 }
