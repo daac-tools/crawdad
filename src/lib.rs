daed6a1f679556a29fa295e6df23c06087358e86
kampersanda <shnsk.knd@gmail.com>
2022-04-01 17:34:08 +0900

    add configs

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,11 @@
 //! Crawdad is a library of natural language dictionaries using character-wise double-array tries.
 //! The implementation is optimized for strings of multibyte-characters,
 //! and you can enjoy fast text processing on such strings such as Japanese or Chinese.
+#![deny(missing_docs)]
+#![cfg_attr(not(feature = "std"), no_std)]
+#[cfg(target_pointer_width = "16")]
+compile_error!("`target_pointer_width` must be larger than or equal to 32");
+
 mod builder;
 pub mod errors;
 pub mod fmptrie;
