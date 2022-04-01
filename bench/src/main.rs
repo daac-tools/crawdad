a5a8a51294ad8562d8b7dc820fef25714eb27bc1
kampersanda <shnsk.knd@gmail.com>
2022-04-01 02:00:22 +0000

    add exact match

--- bench/src/main.rs
+++ bench/src/main.rs
@@ -9,6 +9,9 @@ use crawdad::Statistics;
 
 use clap::Parser;
 
+const TRIALS: usize = 10;
+const SAMPLES: usize = 1000;
+
 #[derive(Parser, Debug)]
 #[clap(name = "bench", about = "A program to measure the performance.")]
 struct Args {
@@ -20,7 +23,7 @@ struct Args {
 }
 
 macro_rules! crawdad_common {
-    ($trie:ident, $keys:ident, $texts:ident) => {
+    ($trie:ident, $keys:ident, $queries:ident, $texts:ident) => {
         let start = Instant::now();
         let trie = crawdad::$trie::from_keys(&$keys).unwrap();
         let duration = start.elapsed();
@@ -29,11 +32,42 @@ macro_rules! crawdad_common {
         println!("vacant_ratio: {:.3}", trie.vacant_ratio());
         println!("constr_sec: {:.3}", duration.as_secs_f64());
 
-        if let Some(texts) = $texts.as_ref() {
+        {
+            // Warmup
+            let mut dummy = 0;
+            for q in &$queries {
+                dummy += trie.exact_match(q).unwrap();
+            }
+            // Measure
             let start = Instant::now();
+            for _ in 0..TRIALS {
+                for q in &$queries {
+                    dummy += trie.exact_match(q).unwrap();
+                }
+            }
+            let duration = start.elapsed();
+            println!(
+                "exact_match: {:.3} [us/query]",
+                duration.as_secs_f64() * 1000000. / TRIALS as f64 / SAMPLES as f64
+            );
+            println!("dummy: {}", dummy);
+        }
+
+        if let Some(texts) = $texts.as_ref() {
+            // Warmup
             let mut dummy = 0;
             let mut mapped = Vec::with_capacity(256);
-            for _ in 0..10 {
+            for text in texts {
+                trie.map_text(text, &mut mapped);
+                for i in 0..mapped.len() {
+                    for m in trie.common_prefix_searcher(&mapped[i..]) {
+                        dummy += i + m.end() + m.value() as usize;
+                    }
+                }
+            }
+            // Measure
+            let start = Instant::now();
+            for _ in 0..TRIALS {
                 for text in texts {
                     trie.map_text(text, &mut mapped);
                     for i in 0..mapped.len() {
@@ -44,7 +78,10 @@ macro_rules! crawdad_common {
                 }
             }
             let duration = start.elapsed();
-            println!("search_sec: {:.3}", duration.as_secs_f64() / 10.);
+            println!(
+                "common_prefix_search: {:.3} [us/text]",
+                duration.as_secs_f64() * 1000000. / TRIALS as f64 / texts.len() as f64
+            );
             println!("dummy: {}", dummy);
         }
     };
@@ -55,6 +92,7 @@ fn main() {
 
     println!("keys_filename: {}", &args.keys_filename);
     let keys = load_file(&args.keys_filename);
+    let queries = random_sample(&keys);
     let texts = if let Some(texts_filename) = args.texts_filename {
         println!("texts_filename: {}", &texts_filename);
         Some(load_file(&texts_filename))
@@ -65,15 +103,15 @@ fn main() {
     println!("#keys: {}", keys.len());
     {
         println!("[crawdad/trie]");
-        crawdad_common!(Trie, keys, texts);
+        crawdad_common!(Trie, keys, queries, texts);
     }
     {
         println!("[crawdad/mptrie]");
-        crawdad_common!(MpTrie, keys, texts);
+        crawdad_common!(MpTrie, keys, queries, texts);
     }
     {
         println!("[crawdad/fmptrie]");
-        crawdad_common!(FmpTrie, keys, texts);
+        crawdad_common!(FmpTrie, keys, queries, texts);
     }
     {
         println!("[yada]");
@@ -89,12 +127,44 @@ fn main() {
         .unwrap();
         let duration = start.elapsed();
         print_heap_bytes(data.len());
+        println!("num_elems: {}", data.len() / 4);
         println!("constr_sec: {:.3}", duration.as_secs_f64());
 
-        if let Some(texts) = texts.as_ref() {
-            let start = Instant::now();
+        let da = yada::DoubleArray::new(data);
+        {
+            // Warmup
             let mut dummy = 0;
-            let da = yada::DoubleArray::new(data);
+            for q in &queries {
+                dummy += da.exact_match_search(q).unwrap();
+            }
+            // Measure
+            let start = Instant::now();
+            for _ in 0..TRIALS {
+                for q in &queries {
+                    dummy += da.exact_match_search(q).unwrap();
+                }
+            }
+            let duration = start.elapsed();
+            println!(
+                "exact_match: {:.3} [us/query]",
+                duration.as_secs_f64() * 1000000. / TRIALS as f64 / SAMPLES as f64
+            );
+            println!("dummy: {}", dummy);
+        }
+
+        if let Some(texts) = texts.as_ref() {
+            // Warmup
+            let mut dummy = 0;
+            for text in texts {
+                let text_bytes = text.as_bytes();
+                for i in 0..text_bytes.len() {
+                    for (id, length) in da.common_prefix_search(&text_bytes[i..]) {
+                        dummy += i + length + id as usize;
+                    }
+                }
+            }
+            // Measure
+            let start = Instant::now();
             for _ in 0..10 {
                 for text in texts {
                     let text_bytes = text.as_bytes();
@@ -106,7 +176,10 @@ fn main() {
                 }
             }
             let duration = start.elapsed();
-            println!("search_sec: {:.3}", duration.as_secs_f64() / 10.);
+            println!(
+                "common_prefix_search: {:.3} [us/text]",
+                duration.as_secs_f64() * 1000000. / TRIALS as f64 / texts.len() as f64
+            );
             println!("dummy: {}", dummy);
         }
     }
@@ -127,4 +200,9 @@ where
     let file = File::open(path).unwrap();
     let buf = BufReader::new(file);
     buf.lines().map(|line| line.unwrap()).collect()
+}
+
+fn random_sample(keys: &[String]) -> Vec<String> {
+    let mut rng = rand::thread_rng();
+    rand::seq::sample_slice(&mut rng, keys, SAMPLES)
 }
