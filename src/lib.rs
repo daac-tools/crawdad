c9455fbd26e35315effd1c10efb66e129790fa76
kampersanda <shnsk.knd@gmail.com>
2022-04-05 08:50:10 +0000

    minor

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@
 //! and you can enjoy fast text processing on such strings such as Japanese or Chinese.
 #![deny(missing_docs)]
 #![cfg_attr(not(feature = "std"), no_std)]
+
 #[cfg(target_pointer_width = "16")]
 compile_error!("`target_pointer_width` must be larger than or equal to 32");
 
