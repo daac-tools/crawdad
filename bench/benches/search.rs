4378d0e59ee9ffe4d24f3866023c6dc31f6af81a
Shunsuke Kanda <shnsk.knd@gmail.com>
2022-04-09 11:03:05 +0900

    Update bench/benches/search.rs
    Co-authored-by: Koichi Akabe <vbkaisetsu@gmail.com>

--- bench/benches/search.rs
+++ bench/benches/search.rs
@@ -249,14 +249,13 @@ fn fst_common_prefix_search<'a>(
         .scan(
             (0, fst.root(), fst::raw::Output::zero()),
             move |(pattern_len, node, output), &byte| {
-                if let Some(b_index) = node.find_input(byte) {
+                node.find_input(byte).map(|b_index| {
                     let transition = node.transition(b_index);
                     *pattern_len += 1;
                     *output = output.cat(transition.out);
                     *node = fst.node(transition.addr);
-                    return Some((node.is_final(), *pattern_len, output.value()));
-                }
-                None
+                    (node.is_final(), *pattern_len, output.value())
+                })
             },
         )
         .filter_map(|(is_final, pattern_len, pattern_id)| {
