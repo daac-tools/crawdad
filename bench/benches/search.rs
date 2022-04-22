bbfee33141262ddc97e4829d90e6d7f60d31276d
Shunsuke Kanda <shnsk.knd@gmail.com>
2022-04-23 00:05:57 +0900

    Update bench/benches/search.rs
    Co-authored-by: Koichi Akabe <vbkaisetsu@gmail.com>

--- bench/benches/search.rs
+++ bench/benches/search.rs
@@ -88,7 +88,7 @@ fn add_exact_match_benches(
     group.bench_function("std/HashMap", |b| {
         let mut map = std::collections::HashMap::new();
         for (i, key) in keys.iter().enumerate() {
-            map.insert(key.clone(), i as u32);
+            map.insert(key, i as u32);
         }
         b.iter(|| {
             let mut dummy = 0;
