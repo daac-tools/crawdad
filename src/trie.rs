5ae5762dab7e5e1a3004e9319b365122073cbed6
kampersanda <shnsk.knd@gmail.com>
2026-08-11 22:31:07 +0900

    Remove redundant single-key trie tests

--- src/trie.rs
+++ src/trie.rs
@@ -410,18 +410,6 @@ mod tests {
 
     #[test]
     fn test_single_key() {
-        let trie = Trie::from_keys(["a"]).unwrap();
-        assert_eq!(trie.exact_match("a".chars()), Some(0));
-        assert_eq!(trie.exact_match("".chars()), None);
-        assert_eq!(trie.exact_match("b".chars()), None);
-        assert_eq!(trie.exact_match("ab".chars()), None);
-
-        let matches: Vec<_> = trie.common_prefix_search("ab".chars()).collect();
-        assert_eq!(matches, vec![(0, 1)]);
-    }
-
-    #[test]
-    fn test_single_long_key() {
         let trie = Trie::from_keys(["世界中"]).unwrap();
         assert_eq!(trie.exact_match("世界中".chars()), Some(0));
         assert_eq!(trie.exact_match("世".chars()), None);
